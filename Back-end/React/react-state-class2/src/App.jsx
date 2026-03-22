cd63324433cff08119f6e1a7453dabc8bcd0f7e8
Nirma J Mali <nirmamali8618@gmail.com>
2026-03-23 00:35:38 +0530

    Lottery game using basic react

--- Back-end/React/react-state-class2/src/App.jsx
+++ Back-end/React/react-state-class2/src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
 import TodoList from './TodoList'
+import Lottery from './Lottery'
 
 function App() {
 
   return (
     <>
-      <TodoList />
+      <Lottery />
     </>
   )
 }
