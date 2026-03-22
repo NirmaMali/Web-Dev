7834cdd154d176928fa2b56418586a2abdf3a5b9
Nirma J Mali <nirmamali8618@gmail.com>
2026-03-23 02:25:58 +0530

    Forms in react

---
+++ Back-end/React/forms-in-react/src/Form.jsx
@@ -0,0 +1,15 @@
+import {useState} from 'react'
+export default function Form(){
+    let [fullName, setFullName] = useState('');
+
+    let handleNameChange =(event)=>{
+        setFullName(event.target.value);
+    }
+
+    return (
+        <form action="">
+            <input placeholder="enter full name" type='text' value={fullName} onChange={handleNameChange} />
+            <button>Submit</button>
+        </form>
+    )
+}
