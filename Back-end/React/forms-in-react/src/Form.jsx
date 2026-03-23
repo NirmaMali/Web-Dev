951232359cfd6476c79d32f04ff5777d7697cf1a
Nirma J Mali <nirmamali8618@gmail.com>
2026-03-23 10:53:00 +0530

    Handling multiple inputs in react form

--- Back-end/React/forms-in-react/src/Form.jsx
+++ Back-end/React/forms-in-react/src/Form.jsx
@@ -1,14 +1,48 @@
-import {useState} from 'react'
-export default function Form(){
-    let [fullName, setFullName] = useState('');
+import { useState } from 'react'
+export default function Form() {
+    let [formData, setFormData] = useState({
+        fullName: '',
+        username: '',
+        password: ''
+    })
 
-    let handleNameChange =(event)=>{
-        setFullName(event.target.value);
+    // let handleNameChange = (event) => {
+    //     setFullName(event.target.value);
+    // }
+
+    // let handleUserChange = (event) => {
+    //     setUsername(event.target.value);
+    // }
+
+    let handleInputChange = (event) => {
+
+        setFormData((currData) => {
+            return { ...currData, [event.target.name]: event.target.value }
+        })
+
     }
 
+    let handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(formData);
+        setFormData({
+            fullName: '',
+            username: '',
+            password: ''
+        });
+    };
+
     return (
-        <form action="">
-            <input placeholder="enter full name" type='text' value={fullName} onChange={handleNameChange} />
+        <form onSubmit={handleSubmit}>
+
+            <label htmlFor="fullName">Full name</label>
+            <input placeholder="enter full name" type='text' value={formData.fullName} onChange={handleInputChange} id="fullName" name='fullName' />
+            <br /><br /> <br /> <br />
+            <label htmlFor="username">User name</label>
+            <input placeholder="enter user name" type='text' value={formData.username} onChange={handleInputChange} id="username" name='username' />
+            <br /><br /> <br /> <br />
+            <label htmlFor="password">Password</label>
+            <input placeholder="enter password" type='password' value={formData.password} onChange={handleInputChange} id="password" name='password' />
             <button>Submit</button>
         </form>
     )
